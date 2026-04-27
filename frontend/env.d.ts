b27a6c9eb260fdea50753324599bbf50f308527b
purpleswtr <sergeenko.mihail@yandex.ru>
2026-04-28 00:25:54 +0300

    pywebview ts inerface

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+
+interface Window {
+  pywebview: {
+    api: {
+      open_file_dialog: () => Promise<string[] | null>
+      add_new_application: (path: string) => Promise<void>
+      get_all_applications: () => Promise<
+        Array<{
+          id: number
+          title: string
+          icon: string
+          value: number
+          process_name?: string
+        }>
+      >
+      set_notifications: (enabled: boolean) => void
+    }
+  }
+}
